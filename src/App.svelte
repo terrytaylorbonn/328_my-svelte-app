c37e894c3f24772cff00d3663520623349a4fbf4
terrytaylorbonn <terrytaylorbonn@gmail.com>
2025-02-28 12:43:01 -0500

    first

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+  let name = "World";
+</script>
+
+<main>
+  <h1>Hello {name}!</h1>
+  <input bind:value={name} placeholder="TT Enter your name" />
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 2rem;
+    font-family: Arial, sans-serif;
+  }
+  input {
+    margin-top: 1rem;
+    padding: 0.5rem;
+    font-size: 1rem;
+  }
+</style>
